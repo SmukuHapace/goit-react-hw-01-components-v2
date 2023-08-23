0d1122d4b41f756d1e9fbfbd0ec15303b267d8f8
Mateusz Kubiak <smuku.mk@gmail.com>
2023-08-23 14:41:11 +0200

    add propTypes to Profile component; start Statistics component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,18 @@
 import { Profile } from './Profile';
 import user from './Profile/user.json';
+// import { Statistics } from './Statistics';
 
 export const App = () => {
   return (
-    <Profile
-      username={user.username}
-      tag={user.tag}
-      location={user.location}
-      avatar={user.avatar}
-      stats={user.stats}
-    />
+    <>
+      <Profile
+        username={user.username}
+        tag={user.tag}
+        location={user.location}
+        avatar={user.avatar}
+        stats={user.stats}
+      />
+      {/* <Statistics /> */}
+    </>
   );
 };
